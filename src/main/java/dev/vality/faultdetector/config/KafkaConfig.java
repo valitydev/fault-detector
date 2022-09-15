2b5f8bc4271acf0be08aaf5c8050b019862a998a
ivnmrtk <ivnmrtk@gmail.com>
2022-09-15 16:17:29 +0300

    rm unused /TD-336

--- src/main/java/dev/vality/faultdetector/config/KafkaConfig.java
+++ src/main/java/dev/vality/faultdetector/config/KafkaConfig.java
@@ -8,14 +8,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
